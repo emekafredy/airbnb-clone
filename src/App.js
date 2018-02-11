9a20bcef7889c820fcccae6f3be3187b562f3944
Emeka Chinedu <chinedufrderick@gmail.com>
2018-02-11 22:59:13 +0100

    Added props and state in component. Also set up the component for fecthing json data

--- src/App.js
+++ src/App.js
@@ -1,18 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Flat from './components/flat';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      flats: [
+
+      ]
+    }
+  }
+
+  componentDidMount() {
+    const url = "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json";
+    fetch(url)
+      .then(response => response.json())
+      //get callback
+      .then((data) => {
+        this.setState({
+          flats: data
+        });
+      })
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      
+      <div className="container"><br/>
+        <div className="App">               
+          <div className="main">
+            <div className="input-group search">
+              <input className="form-control" placeholder="search..."/>
+            </div>
+            
+            <div className="flats">            
+                {this.state.flats.map((flat) => {
+                  return <Flat flat={flat} />
+                })}
+            </div>
+          </div>
+
+          <div className="map">
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/flat.js
+++ src/components/flat.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import './flat.css';
 
 class Flat extends React.Component {
     render() {
+        const title = this.props.flat.price + this.props.flat.priceCurrency + " - " + this.props.flat.name;
+        const style = {
+            backgroundImage : `url(' ${this.props.flat.imageUrl} ')`
+        }
+
         return(
             <div className="flat">
-                <div></div>
-                <div></div>
+                <div className="flat-picture" style={style}></div>
+                <div className="flat-title"> 
+                    {title} 
+                </div>
             </div>
         );
     }
